59c6173b50ed7ef3df631921551d5e5ff888bb3f
anntasziii <anntesziii.kushnir@gmail.com>
2024-11-13 08:29:44 +0100

    Program

---
+++ lab1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Lab1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string inputFilePath = args.Length > 0 ? args[0] : "INPUT.TXT";
+            string outputFilePath = args.Length > 1 ? args[1] : "OUTPUT.TXT";
+            string[] input = File.ReadAllLines(inputFilePath);
+            int N = int.Parse(input[0]);
+            int[] prices = input[1].Split(' ').Select(int.Parse).ToArray();
+
+            int[] maxPricesFromToday = new int[N];
+            maxPricesFromToday[N - 1] = prices[N - 1];
+
+            for (int i = N - 2; i >= 0; i--)
+            {
+                maxPricesFromToday[i] = Math.Max(prices[i], maxPricesFromToday[i + 1]);
+            }
+
+            int totalProfit = 0;
+            int hairLength = 0;
+
+            for (int i = 0; i < N; i++)
+            {
+                hairLength++;
+                if (prices[i] == maxPricesFromToday[i])
+                {
+                    totalProfit += hairLength * prices[i];
+                    hairLength = 0;
+                }
+            }
+            File.WriteAllText(outputFilePath, totalProfit.ToString());
+        }
+    }
+}
